8edea6c1d52c1d59e35e2f7d6ac1c4354747a15b
Chean Botum <142730723+CheanBotum@users.noreply.github.com>
2025-01-09 21:55:17 +0700

    add shop item

--- lib/data/shop_item.dart
+++ lib/data/shop_item.dart
@@ -1,20 +1,4 @@
 
-// // shop_item.dart
-// class ShopItem {
-//   final String id;
-//   final String name;
-//   final String imageUrl;
-//   final double price;
-//
-//   ShopItem({
-//     required this.id,
-//     required this.name,
-//     required this.imageUrl,
-//     required this.price,
-//   });
-// }
-
-
 import 'package:flutter/material.dart';
 
 class ShopItem {
@@ -34,14 +18,13 @@ class ShopItem {
 }
 
 class ShopData with ChangeNotifier {
-  // In-memory data storage (replace with Firebase or local database as needed)
   final List<ShopItem> _items = [];
 
   List<ShopItem> get items => [..._items];
 
   void addItem(ShopItem item) {
     _items.add(item);
-    notifyListeners(); // Notify UI to update
+    notifyListeners();
   }
 
   void removeItem(String id) {
